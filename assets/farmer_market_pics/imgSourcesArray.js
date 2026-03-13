9a424d194d3fe09f9a91b4b7818a58d062da7cc0
Theo <theo.riberti@gmail.com>
2026-03-13 20:50:52 +0000

    Use GitHub raw URLs for all image sources

---
+++ assets/farmer_market_pics/imgSourcesArray.js
@@ -0,0 +1,10 @@
+const BASE = "https://raw.githubusercontent.com/thcero/abcFruits/main/assets/farmer_market_pics/";
+
+const imgSourcesArray = [
+  { uri: BASE + "flea-market-1324565.jpg" },
+  { uri: BASE + "food-market-in-madeira-1329185.jpg" },
+  { uri: BASE + "fruits-1322390.jpg" },
+  { uri: BASE + "fruits-1322729.jpg" },
+  { uri: BASE + "fruits-1323907.jpg" },
+];
+export default imgSourcesArray;
---
+++ assets/users_prof_pics/imgSourcesArray.js
@@ -0,0 +1,18 @@
+const BASE = "https://raw.githubusercontent.com/thcero/abcFruits/main/assets/users_prof_pics/";
+
+const imgSourcesArray = [
+  { uri: BASE + "avatar_0_0.png" },
+  { uri: BASE + "pic.jpg" },
+  { uri: BASE + "pic1.png" },
+  { uri: BASE + "pic2.png" },
+  { uri: BASE + "pic3.png" },
+  { uri: BASE + "pic4.png" },
+  { uri: BASE + "pic5.png" },
+  { uri: BASE + "pic6.png" },
+  { uri: BASE + "pic7.png" },
+  { uri: BASE + "pic8.png" },
+  { uri: BASE + "pic9.png" },
+  { uri: BASE + "pic10.png" },
+  { uri: BASE + "pic11.png" },
+];
+export default imgSourcesArray;
