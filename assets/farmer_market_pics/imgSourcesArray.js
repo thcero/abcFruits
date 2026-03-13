567094ac943c8fb2a66301f08d6352f2ae74e23a
Theo <theo.riberti@gmail.com>
2026-03-13 20:46:16 +0000

    Remove binary assets for Snack compatibility

--- assets/farmer_market_pics/imgSourcesArray.js
+++
@@ -1,8 +0,0 @@
-const imgSourcesArray = [
-  require("./flea-market-1324565.jpg"),
-  require("./food-market-in-madeira-1329185.jpg"),
-  require("./fruits-1322390.jpg"),
-  require("./fruits-1322729.jpg"),
-  require("./fruits-1323907.jpg"),
-];
-export default imgSourcesArray;
